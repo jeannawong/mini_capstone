387890e674e329221e98454d0f39f86328154902
jeannawong <jeannawong25@gmail.com>
2021-01-14 21:28:56 -0600

    From 1/12 to 1/14 class

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,38 @@
 class Api::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+    render "index.json.jb"
+  end
+
+  def show
+    @product = Product.find_by(id: params["id"])
+    render "show.json.jb"
+  end
+
+  def create
+    @product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["prep_time"],
+      description: params["description"],
+    )
+    @product.save
+    render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+    @product.title = params["name"] || @product.title
+    @product.chef = params["price"] || @product.chef
+    @product.prep_time = params["image_url"] || @product.prep_time
+    @product.ingredients = params["description"] || @product.ingredients
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find_by(id: params["id"])
+    product.destroy
+    render json: { message: "product successfully destroyed!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,11 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  namespace :api do
+    get "/products" => "products#index"
+    post "/products" => "products#create"
+    get "/products/:id" => "products#show"
+    patch "/products/:id" => "products#update"
+    delete "/products/:id" => "products#destroy"
+  end
 end
