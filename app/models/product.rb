8dc0ff20e6cf3f9297346b7648dcbd45751f36b9
jeannawong <jeannawong25@gmail.com>
2021-01-17 11:47:29 -0600

    Added new methods in Model.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    price <= 10
+  end
+
+  def tax
+    price * 0.09
+    # if you define a new variable, you have to RETURN it in the code
+  end
+
+  def total
+    price + tax
+  end
 end
